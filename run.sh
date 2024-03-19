4b6cda3bb365f1b48875d514c3cd3dfa35dc97bd
farziana <164096881+farziana@users.noreply.github.com>
2024-03-20 05:48:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-ebfe3a14-7871-49bf-9a62-e0423a3fcbcd/)
 
 
 echo "$curl_output" >> Update.tsx
